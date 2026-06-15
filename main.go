774d7993e549755fe87208b0c10cee7831af4824
Gibbsface <cole.gibbs@gmail.com>
2026-06-15 13:24:46 -0500

    added healthz, metrics, and reset endpoints

--- main.go
+++ main.go
@@ -1,13 +1,52 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (c *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	sMux := http.NewServeMux()
+	cfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
+	sMux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+
+	sMux.HandleFunc("GET /metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "Hits: %v", cfg.fileserverHits.Load())
+	})
+
+	sMux.HandleFunc("POST /reset", func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Swap(0)
+		w.WriteHeader(200)
+	})
+
+	sMux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+
 	s := &http.Server{
 		Addr:    ":8080",
 		Handler: sMux,
 	}
 
 	s.ListenAndServe()
+
 }
