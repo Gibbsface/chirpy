4baa09974dc65d352e1017fc7a9cb0a26561a190
Gibbsface <cole.gibbs@gmail.com>
2026-06-16 13:17:29 -0500

    added gitignore

--- main.go
+++ main.go
@@ -23,19 +23,24 @@ func main() {
 		fileserverHits: atomic.Int32{},
 	}
 
-	sMux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+	sMux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
 	})
 
-	sMux.HandleFunc("GET /metrics", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	sMux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html; charset=utf-8")
 		w.WriteHeader(200)
-		fmt.Fprintf(w, "Hits: %v", cfg.fileserverHits.Load())
+		fmt.Fprintf(w, `<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileserverHits.Load())
 	})
 
-	sMux.HandleFunc("POST /reset", func(w http.ResponseWriter, r *http.Request) {
+	sMux.HandleFunc("POST /admin/reset", func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits.Swap(0)
 		w.WriteHeader(200)
 	})
