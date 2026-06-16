206079f49601cf52dad342d856a217a5568ee66d
Gibbsface <cole.gibbs@gmail.com>
2026-06-16 17:03:47 -0500

    API can now validate chirp using ApiValidateChirp()

--- api_handlers.go
+++ api_handlers.go
@@ -2,49 +2,68 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"time"
 )
+
+type requestJSON struct {
+	Body string `json:"body"`
+}
+
+type responseJSON struct {
+	Valid bool `json:"valid"`
+}
+
+type errJSON struct {
+	Error string `json:"error"`
+}
+
+func ApiValidateChirp(w http.ResponseWriter, r *http.Request) {
+
+	// attempt to decode JSON from request
+	decoder := json.NewDecoder(r.Body)
+	reqJSON := requestJSON{}
+	err := decoder.Decode(&reqJSON)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Something went wrong")
+		return
+	}
+
+	// at this point, we know reqJSON successfully represents the request
+	if len(reqJSON.Body) > 140 {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	// handle valid response
+	respondWithJSON(w, http.StatusOK, responseJSON{
+		Valid: true,
+	})
+
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload any) {
+	// attempt to marshal
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshaling JSON: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, errJSON{
+		Error: message,
+	})
+}
 
 func ApiHealthz(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
-}
-
-func ApiValidateChirp(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Body string `json:"body"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	params := parameters{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		log.Printf("Error decoding parameters: %s", err)
-		w.WriteHeader(500)
-		fmt.Fprintf(w, "")
-		return
-	}
-
-	type returnVals struct {
-		CreatedAt time.Time `json:"created_at"`
-		ID        int       `json:"id"`
-	}
-	respBody := returnVals{
-		CreatedAt: time.Now(),
-		ID:        123,
-	}
-	dat, err := json.Marshal(respBody)
-	if err != nil {
-		log.Printf("Error marshalling JSON: %s", err)
-		w.WriteHeader(500)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	w.Write(dat)
-
 }
